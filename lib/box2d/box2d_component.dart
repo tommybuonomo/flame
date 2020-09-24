6d14a0bcc7c67b58b7bef12a651a2a445fc93f4f
Tommy Buonomo <tommy.buonomo@lydia-app.com>
2020-09-24 20:29:46 +0200

    Fix breaking changes

--- lib/box2d/box2d_component.dart
+++ lib/box2d/box2d_component.dart
@@ -34,8 +34,7 @@ abstract class Box2DComponent extends Component {
     double scale = DEFAULT_SCALE,
   }) {
     dimensions ??= window.physicalSize;
-    final pool = DefaultWorldPool(worldPoolSize, worldPoolContainerSize);
-    world = World.withPool(Vector2(0.0, gravity), pool);
+    world = World.withGravity(Vector2(0.0, gravity));
     viewport = Viewport(dimensions, scale);
   }
 
@@ -140,10 +139,10 @@ abstract class BodyComponent extends Component {
 
   void _renderChain(Canvas canvas, Fixture fixture) {
     final ChainShape chainShape = fixture.getShape();
-    final List<Vector2> vertices = Vec2Array().get(chainShape.getVertexCount());
+    final List<Vector2> vertices = List(chainShape.getVertexCount());
 
     for (int i = 0; i < chainShape.getVertexCount(); ++i) {
-      body.getWorldPointToOut(chainShape.getVertex(i), vertices[i]);
+      vertices[i] = body.getWorldPoint(chainShape.getVertex(i));
       vertices[i] = viewport.getWorldToScreen(vertices[i]);
     }
 
@@ -165,7 +164,7 @@ abstract class BodyComponent extends Component {
   void _renderCircle(Canvas canvas, Fixture fixture) {
     var center = Vector2.zero();
     final CircleShape circle = fixture.getShape();
-    body.getWorldPointToOut(circle.p, center);
+    center = body.getWorldPoint(circle.position);
     center = viewport.getWorldToScreen(center);
     renderCircle(
         canvas, Offset(center.x, center.y), circle.radius * viewport.scale);
@@ -180,10 +179,10 @@ abstract class BodyComponent extends Component {
   void _renderPolygon(Canvas canvas, Fixture fixture) {
     final PolygonShape polygon = fixture.getShape();
     assert(polygon.count <= MAX_POLYGON_VERTICES);
-    final List<Vector2> vertices = Vec2Array().get(polygon.count);
+    final List<Vector2> vertices = List(polygon.count);
 
     for (int i = 0; i < polygon.count; ++i) {
-      body.getWorldPointToOut(polygon.vertices[i], vertices[i]);
+      vertices[i] = body.getWorldPoint(polygon.vertices[i]);
       vertices[i] = viewport.getWorldToScreen(vertices[i]);
     }
 
